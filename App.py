ae5955ef55dd5c1aee9d01562187f2a5ca0750ba
rgerritsen <rgerritsen98@outlook.com>
2022-03-18 12:49:30 -0500

    Update App.py

--- App.py
+++ App.py
@@ -11,7 +11,8 @@ from kivy.graphics import Color, Ellipse, Line, Rectangle
 from kivy.uix.widget import Widget
 from PIL import Image
 import imagehash
-
+from kivy.core.window import Window
+Window.fullscreen = True
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 class Global():
